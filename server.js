ab882c51f04813ef0a522d95ee6a8910b6091553
Arihant Chhajed <arihant_chhajed@yahoo.in>
2017-03-21 00:14:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
-    var result = 'App is running'
+    var result = 'Name:Arcsoftech-Webhook./nAuthor:Arihant Chhajed./nStatus:App is running./n'
     response.send(result);
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
