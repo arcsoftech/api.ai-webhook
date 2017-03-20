020fd07ddbe29a8af67293c7644d32f9cef60ca6
Arihant Chhajed <arihant_chhajed@yahoo.in>
2017-03-20 20:15:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -62,11 +62,12 @@ WeatherResult=request(options, function (error, response, body) {
   // });
 })
 
-var server = app.listen(Number(process.env.port) || 8080, function () {
+app.set('port', (process.env.PORT || 5000));
 
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log("Example app listening at http://%s:%s", host, port);
-
-})
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
